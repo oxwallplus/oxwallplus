922358d6306d23bb1f0331e91ad5894b6daebf7e
toar86 <kontakt@artmedia.biz.pl>
2021-03-14 13:40:51 +0100

    remove install dir

--- ow_install/controllers/error.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class INSTALL_CTRL_Error extends INSTALL_ActionController
-{
-    public function notFound() {
-        $this->redirect(OW::getRouter()->urlForRoute('intro'));
-    }
-}
--- ow_install/view/controllers/install_intro.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-    <div class="col-lg-12">
-        <?php echo __('Thank you for select Oxwall Plus!'); ?><br />
-        <?php echo __('Oxwall Plus is unbelievably flexible and easy to use PHP/MySQL social networking software platform. Oxwall Plus is used for a wide range of projects starting from family sites and custom social networks to collaboration tools and enterprise community solutions.'); ?><br />
-        <?php echo __('Go ahead and complete instalation Plus.'); ?><br />
-        <br />
-    </div>
-    <a href="<?php echo OW::getRouter()->urlForRoute("requirements"); ?>" role="button" class="btn btn-block btn-lg btn-primary"><?php echo __('Continue'); ?></a>
-</div>
